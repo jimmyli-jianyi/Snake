7d897fc7005235c44c81dc1ff132a317f2c8f139
Jimmy Li <jimmy.jianyil@gmail.com>
2018-10-02 02:08:27 -0700

    uploading previous projects

---
+++ snake.js
@@ -0,0 +1,201 @@
+const SCALE = 20;
+const WIDTH = 30;
+const HEIGHT = 30;
+const SPEED = 150;
+
+/** One-time setup: find HTML canvas element */
+
+const canvas = document.getElementById('board');
+canvas.setAttribute('height', HEIGHT * SCALE);
+canvas.setAttribute('width', WIDTH * SCALE);
+const ctx = canvas.getContext('2d');
+
+/** Point: */
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(this.x * SCALE, this.y * SCALE, SCALE / 2, 0, Math.PI * 2);
+    ctx.fill();
+  }
+
+  static newRandom() {
+    const randRange = (low, hi) => low + Math.floor(Math.random() * (hi - low));
+    return new Point(randRange(1, WIDTH), randRange(1, HEIGHT));
+  }
+
+  isOutOfBound() {
+    return this.x <= 0 || this.x >= WIDTH || this.y <= 0 || this.y >= HEIGHT;
+  }
+}
+
+/** Food pellet */
+
+class Pellet {
+  constructor(x, y) {
+    this.pt = new Point(x, y);
+  }
+
+  static newRandom() {
+    const pt = Point.newRandom();
+    return new Pellet(pt.x, pt.y);
+  }
+
+  draw() {
+    this.pt.draw('green');
+  }
+}
+
+/** Snake */
+
+class Snake {
+  constructor(keymap, start, dir, color) {
+    this.color = color;
+    this.keymap = keymap; // mapping of keys to directions
+    this.parts = [start]; // list of x-y coordinates of parts
+    this.dir = dir; // dir to move on next move
+    this.growBy = 0; // how many to grow by (goes up after eating)
+  }
+
+  draw() {
+    for (const p of this.parts) p.draw(this.color);
+  }
+
+  notAppearInside(pt) {
+    return this.parts.some(me => me.x === pt.x && me.y === pt.y);
+  }
+
+  contains(pt) {
+    let [a, ...rest] = this.parts;
+    return rest.some(me => me.x === pt.x && me.y === pt.y);
+  }
+
+  crashIntoSelf() {
+    return this.contains(this.head());
+  }
+
+  crashIntoWall() {
+    return this.head().isOutOfBound();
+  }
+
+  head() {
+    return this.parts[0];
+  }
+
+  move() {
+    const { x, y } = this.head();
+    let pt;
+    if (this.dir === 'left') pt = new Point(x - 1, y);
+    if (this.dir === 'right') pt = new Point(x + 1, y);
+    if (this.dir === 'up') pt = new Point(x, y - 1);
+    if (this.dir === 'down') pt = new Point(x, y + 1);
+    this.parts.unshift(pt);
+  }
+
+  handleKey(key) {
+    if (this.keymap[key] !== undefined) this.changeDir(this.keymap[key]);
+  }
+
+  changeDir(dir) {
+    if (
+      (this.dir === 'left' && dir !== 'right') ||
+      (this.dir === 'right' && dir !== 'left') ||
+      (this.dir === 'up' && dir !== 'down') ||
+      (this.dir === 'down' && dir !== 'up')
+    )
+      this.dir = dir;
+  }
+
+  grow() {
+    this.growBy += 1;
+  }
+
+  truncate() {
+    if (this.growBy === 0) this.parts.pop();
+    else this.growBy--;
+  }
+
+  eats(food) {
+    const head = this.head();
+    return food.find(f => f.pt.x === head.x && f.pt.y === head.y);
+  }
+}
+
+/** Overall game. */
+
+class Game {
+  constructor(snake) {
+    this.snake = snake;
+    this.food = [];
+    this.numFood = 3;
+
+    this.interval = null;
+    this.keyListener = this.onkey;
+  }
+
+  refillFood() {
+    while (this.food.length < this.numFood) {
+      let newPellet = Pellet.newRandom();
+      if (!this.snake.notAppearInside(newPellet)) {
+        this.food.push(newPellet);
+      }
+    }
+  }
+
+  play() {
+    document.addEventListener('keydown', e => {
+      this.keyListener(e);
+    });
+    this.interval = window.setInterval(this.tick.bind(this), SPEED);
+  }
+
+  onkey(e) {
+    this.snake.handleKey(e.key);
+  }
+
+  removeFood(pellet) {
+    this.food = this.food.filter(
+      f => f.pt.x !== pellet.pt.x && f.pt.y !== pellet.pt.y
+    );
+  }
+
+  tick() {
+    const dead = this.snake.crashIntoSelf() || this.snake.crashIntoWall();
+
+    if (!dead) {
+      ctx.clearRect(0, 0, SCALE * WIDTH, SCALE * HEIGHT);
+      for (const f of this.food) {
+        f.draw();
+      }
+      let eaten;
+      this.snake.move();
+      this.snake.truncate();
+      this.snake.draw();
+      if ((eaten = this.snake.eats(this.food))) {
+        this.removeFood(eaten);
+        this.snake.grow();
+      }
+      this.refillFood();
+    } else {
+      window.clearInterval(this.interval);
+      window.removeEventListener('keydown', this.keyListener);
+      window.alert('Game Over!');
+    }
+  }
+}
+
+const snake = new Snake(
+  { ArrowLeft: 'left', ArrowRight: 'right', ArrowUp: 'up', ArrowDown: 'down' },
+  new Point(20, 20),
+  'right',
+  'cyan'
+);
+
+const game = new Game(snake);
+game.play();
